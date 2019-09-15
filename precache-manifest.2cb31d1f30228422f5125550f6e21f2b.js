399c983d2fa0b7ad27072994a9ef3e94ee81aedd
Badreddine Gharbi <blackbird@Badreddines-Air.home>
2019-09-15 16:09:16 -0700

    Updates

---
+++ precache-manifest.2cb31d1f30228422f5125550f6e21f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a551f750a49d9d039bd0",
+    "url": "/dad_jokes/static/js/runtime~main.a551f750.js"
+  },
+  {
+    "revision": "7cc25f810f4a1babd967",
+    "url": "/dad_jokes/static/js/main.7cc25f81.chunk.js"
+  },
+  {
+    "revision": "43a33ea9b5862b0054c4",
+    "url": "/dad_jokes/static/js/1.43a33ea9.chunk.js"
+  },
+  {
+    "revision": "7cc25f810f4a1babd967",
+    "url": "/dad_jokes/static/css/main.14b92e98.chunk.css"
+  },
+  {
+    "revision": "30cfdf055c22d16c054f9e60dc750821",
+    "url": "/dad_jokes/index.html"
+  }
+];
